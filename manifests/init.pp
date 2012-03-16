9adb492c1226304ab9be228c4f10d5336d781e2a
Adam Heinz <amh@amh-fc15.localdomain>
2012-03-16 14:04:05 -0400

    Configuring php and iptables for vtigercrm.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,23 @@
 class vtigercrm {
+	include apache2::php
+	include iptables
     include mysql::server
 
 	$version = "5.3.0"
+
+    augeas { "/etc/php.ini":
+        changes => [
+            "set allow_call_time_pass_reference On",
+            "set display_errors On",
+            "set error_reporting \"E_WARNING & ~E_NOTICE & ~E_DEPRECATED\"",
+            "set log_errors Off",
+            "set max_execution_time 600",
+            "set short_open_tag On",
+        ],
+        context => "/files/etc/php.ini/PHP",
+        notify  => Service[httpd],
+        require => Package[php-common],
+    }
 
 	exec { "Download source tarball":
 		command => "wget http://prdownloads.sourceforge.net/vtigercrm/vtigercrm-$version.tar.gz -O /tmp/vtigercrm-$version.tar.gz",
@@ -20,6 +36,13 @@ class vtigercrm {
 		require => Exec["Unzip source tarball"],
 	}
 
+	iptables { http:
+		proto => "tcp",
+		dport => "80",
+		jump  => "ACCEPT",
+		state => "NEW",
+	}
+
     mysql::database { "vtigercrm$version":
     	ensure => present,
     }
@@ -29,5 +52,11 @@ class vtigercrm {
         host     => "localhost",
         password => "vtigercrm$version",
         user     => "vtigercrm$version",
+    }
+
+    package {[ php-common, php-gd, php-imap, php-mysql ]:
+    	ensure => latest,
+    	notify => Service[httpd],
+    	provider => yum,
     }
 }
